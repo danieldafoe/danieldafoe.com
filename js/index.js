057b319a2e9eb259798fb645a8689e77f3339c26
Daniel Dafoe <dannydafoe@gmail.com>
2016-11-14 23:04:05 -0500

    Change over jQuery associated functionality to use vanilla JS

--- js/index.js
+++ js/index.js
@@ -1,27 +1,37 @@
+// ==========================================================================
+// JavaScript for danieldafoe.github.io
+// ==========================================================================
+
 // Nothing groundbreaking to see here. Sorry.
 
 // Avoid naming collisions
 'use strict';
 
-$(document).ready(function() {
+// Globally declare frequent element retrievals
+var nav = document.querySelector('nav');
+var navLinks = document.querySelectorAll('.nav-inner > a');
+var navBtn = document.querySelector('.nav-reveal button');
+var navReveal = document.querySelector('.nav-reveal');
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
   // Adding some flair to my URL--with pizza!
   window.location.hash = "🍕";
 
   // Clear the form on page load.
   clearForm();
 
-  // Globally declare frequent element retrievals
-  var navLinks = $('.nav-inner > a');
-  var button = $('.nav-reveal button');
-
   // Test adding success class to submit button
-  $('.btn-default').on('click', function(e) {
+  var contactSubmitBtn = document.querySelector('.btn-default');
+  contactSubmitBtn.addEventListener('click', function(e) {
     e.preventDefault();
-    $(this).blur();
-    $('.btn-default').addClass('submitting');
+    this.blur();
+    this.classList.add('submitting');
 
     setTimeout(function() {
-      $('.btn-default').removeClass('submitting').addClass('submitted');
+      contactSubmitBtn.classList.remove('submitting');
+      contactSubmitBtn.classList.add('submitted');
     }, 3000);
   });
 
@@ -29,204 +39,197 @@ $(document).ready(function() {
   //
   //
   // Animation - Navigation
-  $('.nav-reveal').addClass('nav-reveal--in');
+  navReveal.classList.add('nav-reveal--in');
 
   // Animation - Logo - Name
-  $('.logo h1').css({
-                      '-webkit-transform':'translate(0,0)',
-                      '-moz-transform':'translate(0,0)',
-                      '-ms-transform':'translate(0,0)',
-                      '-o-transform':'translate(0,0)',
-                      'transform':'translate(0,0)'
-                    });
+  var logoName = document.querySelector('.logo h1');
+  logoName.style.webkitTransform = 'translate(0,0)';
+  logoName.style.mozTransform = 'translate(0,0)';
+  logoName.style.msTransform = 'translate(0,0)';
+  logoName.style.oTransform = 'translate(0,0)';
+  logoName.style.transform = 'translate(0,0)';
 
   // Animation - Logo - Title
-  $('.logo h2').css({
-                      '-webkit-transform':'translate(0,0)',
-                      '-moz-transform':'translate(0,0)',
-                      '-ms-transform':'translate(0,0)',
-                      '-o-transform':'translate(0,0)',
-                      'transform':'translate(0,0)'
-                    });
+  var logoTitle = document.querySelector('.logo p');
+  logoTitle.style.webkitTransform = 'translate(0,0)';
+  logoTitle.style.mozTransform = 'translate(0,0)';
+  logoTitle.style.msTransform = 'translate(0,0)';
+  logoTitle.style.oTransform = 'translate(0,0)';
+  logoTitle.style.transform = 'translate(0,0)';
+}
 
-  // EVENT LISTENERS
-  //
-  //
-  $(document).on('keydown', function(e) {
-    var key = e.keyCode || e.which;
-
-    if (key === 27) {
-      // ESC key pressed
-      if ($('nav').hasClass('in')) {
-        // Nav is open
-        $('nav').removeClass('in').addClass('out');
-
-        // Accessibility behaviour
-        button.attr('aria-expanded', 'false');
-
-        // Change nav-reveal button colour
-        button.css({color:"#fff"});
-
-        // Change button text
-        button.html('<i class="fa fa-bars"></i> MENU');
-
-        // Bring focus back to button
-        button.focus();
-      }
-    }
-    else if (key === 9) {
-      // Tab key pressed
-      if ($('nav').hasClass('in')) {
-        if (e.target.hash === '#contact') {
-          console.log('Quick, tab back to CLOSE');
-        }
-      }
-    }
-    else if (key === 13) {
-      // Enter key pressed
-      if ((document.activeElement.parentElement.classList).contains('nav-reveal')) {
-        button.focus();
-        // Change tabindex of each nav link
-        navLinks.each(function(e) {
-          $(this).attr('tabindex', -1);
-        });
-      }
-    }
-    else if (key === 38) {
-      if ($('nav').hasClass('in')) {
-        $(document.activeElement).prev('a[tabindex]').focus();
-      }
-    }
-    else if (key === 40) {
-      if ($('nav').hasClass('in')) {
-        $(document.activeElement).next('a[tabindex]').focus();
-      }
-    }
-  });
-
-  $('.nav-reveal').on('touch click', function() {
-    var nav = $('nav');
-
-    if (nav.hasClass('out')) {
-      nav.removeClass('out').addClass('in');
-      button.html('<i class="fa fa-close"></i> CLOSE');
-      
-      // Accessibility behaviour
-      button.attr('aria-expanded', 'true');
-
-      // Change nav-reveal button colour
-      button.css({color:"#1663c7"});
-
-      // Change tabindex of each nav link
-      navLinks.each(function(e) {
-        $(this).attr('tabindex', 0);
-      });
-
-      // Set focus to the first item in the menu
-      $('.nav-inner > a:first-child').focus();
-    }
-    else {
-      nav.removeClass('in').addClass('out');
-      button.html('<i class="fa fa-bars"></i> MENU');
-      
-      // Accessibility behaviour
-      button.attr('aria-expanded', 'false');
-
-      // Change nav-reveal button colour
-      button.css({color:"#fff"});
-
-      // Change tabindex of each nav link
-      navLinks.each(function(e) {
-        $(this).attr('tabindex', -1);
-      });
-
-      // Bring focus back to button
-      button.focus();
-    }
-  });
-
-  // On nav-item click, scroll to div. //
-	$('.nav-inner a').on('touchend click', function(e) {
-		var link = $(this).attr('href');
-		$('html, body').animate({
-	            scrollTop: $(link).offset().top
-	        }, 800);
-
-    // Ensure the menu closes after a click or touch.
-    $('.nav-reveal').click();
-	});
-
-  // CONTACT
-
-  // $('#form-contact').on('submit', function(e) {
-  //   var e = $('#email').val();
-  //   var m = $('#msg').val();
-
-  //   var ajaxData = {
-  //     email: e,
-  //     msg: m
-  //   }
-
-  //   e.preventDefault();
-  //   //return checkForm();
-
-  //   // If checkForm() returns true, the form can be
-  //   // submitted for an ajax call
-  //   if (checkForm) {
-  //     // Make ajax call to php script
-  //     $.ajax({
-  //       url: '../php/send_mail.php',
-  //       data: ajaxData,
-  //       success: function(data) {
-  //         console.log(data);
-  //       },
-  //       error: function(jXHR, textStatus) {
-  //         console.log(jXHR);
-  //         console.log(textStatus);
-  //       }
-  //     })
-  //   }
-  // });
-
-  // SOCIAL
-  // Submit clicks to Google Analytics for tracking
-  // Send a twitter click to GA.
-  $('#twitter-link').on('click', function() {
-    ga('send', 'event', 'button', 'click', 'twitter');
-  });
-
-  // Send a linkedin click to GA.
-  $('#linkedin-link').on('click', function() {
-    ga('send', 'event', 'button', 'click', 'linkedin');
-  });
-
-  // Send a github click to GA.
-  $('#github-link').on('click', function() {
-    ga('send', 'event', 'button', 'click', 'github');
-  });
-  
-
-  // FUNCTION DECLARATIONS
-  //
-  //
-  // Check if the contact form is blank
-  function checkForm() {
-    var email = $('#email').val();
-    var msg = $('#msg').val();
-    
-    if (email === "" ||
-        msg === "") {
-          return false;
-        }
-    else {
-      return true;
-    }
-  }
-  // Function for clearing the form data
-  function clearForm() {
-    $('input, textarea').each(function(i, el) {
-      el.value = "";
-    });
-  }
-// End of document.ready
+//
+// Events
+// --------------------------------------------------------------------------
+document.addEventListener('keydown', function(e) {
+  handleKeydown(e);
 });
+
+function handleKeydown(e) {
+  var key = e.keyCode || e.which;
+
+  if (key === 27) {
+    // ESC key pressed
+    if (navIsOpen()) {
+      closeMenu();
+    }
+  }
+  else if (key === 9) {
+    // Tab key pressed
+    if (navIsOpen()) {
+      if (e.target.hash === '#contact') {
+        // Tabbed from the last link in the menu
+        e.preventDefault();
+        navBtn.focus();
+      }
+      else if (e.target.nodeName === 'BUTTON') {
+        // Tabbed from the menu button
+        e.preventDefault();
+        navLinks[0].focus();
+
+      }
+    }
+  }
+  else if (key === 13) {
+    // Enter key pressed
+    if ((document.activeElement.parentElement.classList).contains('nav-reveal')) {
+      navBtn.focus();
+      // Change tabindex of each nav link
+      for (var i = 0; i < navLinks.length; i++) {
+        navLinks[i].setAttribute('tabindex', -1);
+      }
+    }
+  }
+  else if (key === 38) {
+    if (navIsOpen()) {
+      $(document.activeElement).prev('a[tabindex]').focus();
+    }
+  }
+  else if (key === 40) {
+    if (navIsOpen()) {
+      $(document.activeElement).next('a[tabindex]').focus();
+    }
+  }
+}
+
+function navIsOpen() {
+  return document.querySelector('nav').classList.contains('in');
+}
+function closeMenu() {
+  var nav = document.querySelector('nav');
+
+  if (navIsOpen()) {
+    // Nav is open
+    nav.classList.remove('in');
+    nav.classList.add('out');
+
+    // Accessibility behaviour
+    navBtn.setAttribute('aria-expanded', 'false');
+
+    // Change nav-reveal button colour
+    navBtn.style.color = '#fff';
+
+    // Change button text
+    navBtn.innerHTML = '<i class="fa fa-bars"></i> MENU';
+
+    // Change tabindex of each nav link
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].setAttribute('tabindex', -1);
+    }
+
+    // Bring focus back to button
+    navBtn.focus();
+  }
+}
+function openMenu() {
+  if (!navIsOpen()) {
+    // Nav is open
+    nav.classList.toggle('out');
+    nav.classList.add('in');
+
+    // Accessibility behaviour
+    navBtn.setAttribute('aria-expanded', 'true');
+
+    // Change nav-reveal button colour
+    navBtn.style.color = '#1663c7';
+
+    // Change button text
+    navBtn.innerHTML = '<i class="fa fa-close"></i> CLOSE';
+
+    // Change tabindex of each nav link
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].setAttribute('tabindex', 0);
+    }
+
+    // Set focus to the first item in the menu
+    document.querySelector('.nav-inner > a:first-child').focus();
+  }
+}
+
+navBtn.addEventListener('click', function() {
+  if (!navIsOpen()) {
+    openMenu();
+  }
+  else {
+    closeMenu();
+  }
+})
+
+document.querySelector('.nav-inner').addEventListener('click', handleNavLink);
+
+function handleNavLink() {
+  console.log(this);
+}
+
+$('.nav-inner a').on('touchend click', function(e) {
+	var link = $(this).attr('href');
+	$('html, body').animate({
+            scrollTop: $(link).offset().top
+        }, 800);
+
+  closeMenu();
+});
+
+//
+// Social
+// --------------------------------------------------------------------------
+// Submit clicks to Google Analytics for tracking
+// Send a twitter click to GA.
+document.getElementById('twitter-link').addEventListener('click', recordInteraction('twitter'));
+document.getElementById('linkedin-link').addEventListener('click', recordInteraction('linkedin'));
+document.getElementById('github-link').addEventListener('click', recordInteraction('github'));
+document.getElementById('codepen-link').addEventListener('click', recordInteraction('linkedin'));
+
+function recordInteraction(link) {
+  ga('send', 'event', 'button', 'click', link);
+}
+
+//
+// Contact
+// --------------------------------------------------------------------------
+
+// Check if the contact form is blank
+function checkForm() {
+  var email = $('#email').val();
+  var msg = $('#msg').val();
+  
+  if (email === "" ||
+      msg === "") {
+        return false;
+      }
+  else {
+    return true;
+  }
+}
+// Function for clearing the form data
+function clearForm() {
+  var inputs = document.getElementsByTagName('input');
+  var textareas = document.getElementsByTagName('textarea');
+
+  // for (var i = inputs.length; i >= 0; i--) {
+  //   inputs[i].value = '';
+  // }
+  // for (var i = textareas.length; i >= 0; i--) {
+  //   textareas[i].value = '';
+  // }
+}
