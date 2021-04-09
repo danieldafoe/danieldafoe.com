7ae40796f7cf23f5cba7c0900f180e4f55e08199
Daniel Dafoe <dannydafoe@gmail.com>
2021-04-09 19:59:49 -0400

    feat: test guestbook with Netlify Forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import { setLocalStorage, getLocalStorage } from './helpers';
 
 const APP_CONSTANTS = {
-  ACCEPTED_COOKIES: 'acceptedCookies'
+  ACCEPTED_COOKIES: 'acceptedCookies',
+  NETLIFY_API_BASE: 'https://api.netlify.com/api/v1',
+  NETLIFY_APP_NAME: 'danieldafoe.com',
+  NETLIFY_AUTH_TOKEN: 'LKHbNmOdpIeO08oD6AODvRz44FG83vpBnifFLXLIPF0',
 };
 
 document.addEventListener('DOMContentLoaded', onLoad);
@@ -14,6 +17,8 @@ function onLoad() {
     const cookiesBtn = document.querySelector('aside button');
     cookiesBtn.addEventListener('click', hideCookiesBanner);
   }
+
+  loadNetlifySubmissions();
 }
 
 function acceptedCookiesBanner() {
@@ -24,4 +29,16 @@ function hideCookiesBanner() {
   const banner = document.getElementsByTagName('aside');
   banner[0].classList.toggle('hide');
   setLocalStorage(APP_CONSTANTS.ACCEPTED_COOKIES, 'true');
+}
+
+function loadNetlifySubmissions() {
+  const requestInfo = {
+    headers: new Headers({
+      'Authorization': `Bearer ${APP_CONSTANTS.NETLIFY_AUTH_TOKEN}`,
+      'User-Agent': `${APP_CONSTANTS.NETLIFY_APP_NAME} (dannydafoe@gmail.com)`
+    })
+  };
+
+  const request = new Request(`${APP_CONSTANTS.NETLIFY_API_BASE}/forms/guestbook/submissions`, requestInfo);
+  fetch(request).then((submissions) => console.log());
 }
