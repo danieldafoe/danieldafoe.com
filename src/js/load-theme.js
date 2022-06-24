7add60b32e2015c99ceefbd70f97f651cfcd0c82
Daniel Dafoe <dannydafoe@gmail.com>
2022-06-23 23:17:57 -0400

    feat: optimize render-blocking JS
    - remove unused assets

--- src/js/load-theme.js
+++ src/js/load-theme.js
@@ -1,14 +1,4 @@
 // Get theme from LocalStorage to see if it's dark
-const THEMES = {
-  DARK: 'dark',
-  LIGHT: 'light'
-};
-const localStorage = window.localStorage;
-
-if (localStorage) {
-  const themeChoice = localStorage.getItem('theme');
-
-  if (themeChoice && themeChoice === THEMES.DARK) {
-    document.body.classList.add('dark');
-  }
+if (window.localStorage) {
+  if (localStorage.getItem('theme') === 'dark') document.body.classList.add('dark');
 }
