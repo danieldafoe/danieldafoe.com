3c82064f68caeae98bb03eca937e4701af58287e
Daniel Dafoe <dannydafoe@gmail.com>
2023-02-18 21:41:48 -0800

    test: pass word to email

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,22 +1,22 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const msg = {
-  to: 'dannydafoe@gmail.com',
-  from: 'dannydafoe+rosy@gmail.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-
 exports.handler = async event => {
   try {
+    const msg = {
+      to: 'dannydafoe@gmail.com',
+      from: 'dannydafoe+rosy@gmail.com',
+      template_id: 'd-0e1dc29d693341bb995b78eeb4295002',
+      dynamic_template_data: {
+        word: event.body || 'null'
+      }
+    };
     await sgMail.send(msg);
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: "Let's become serverless conductors!!!"
+        message: "Success"
       })
     };
   } catch (e) {
@@ -24,7 +24,7 @@ exports.handler = async event => {
 
     return {
       statusCode: 500,
-      body: e.mssage
+      body: e.message
     };
   }
 };
